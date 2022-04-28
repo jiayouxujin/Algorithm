de1a2ca5efc0a1823ab9c74b601fa78582ee7e8d
xujin <1319039722@qq.com>
2022-04-28 10:19:47 +0800

    2022-04-28

---
+++ 1004.最大连续-1-的个数-iii.go
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=1004 lang=golang
+ *
+ * [1004] 最大连续1的个数 III
+ */
+
+// @lc code=start
+func longestOnes(nums []int, k int) int {
+	left, right, res := 0, 0, 0
+	for left < len(nums) {
+		if right < len(nums) && ((nums[right] == 0 && k > 0) || nums[right] == 1) {
+			if nums[right] == 0 {
+				k--
+			}
+			right++
+		} else {
+			if right == len(nums) || k == 0 {
+				res = max(res, right-left)
+			}
+
+			if nums[left] == 0 {
+				k++
+			}
+			left++
+		}
+	}
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// @lc code=end
+
