3428e956fa3ab30bd71c45a3e988a70ad47c97b3
xujin <1319039722@qq.com>
2022-05-09 10:06:50 +0800

    2022-05-09

--- 1438.绝对差不超过限制的最长连续子数组-1.go
+++ 1438.绝对差不超过限制的最长连续子数组-1.go
@@ -6,20 +6,17 @@
 
 // @lc code=start
 func longestSubarray(nums []int, limit int) int {
-	minStack, maxStack := []int{}, []int{}
-	left, res := 0, 0
-	for right, n := range nums {
-		for len(minStack) > 0 && nums[minStack[len(minStack)-1]] > n {
+	minStack, maxStack, left, res := []int{}, []int{}, 0, 0
+	for right, num := range nums {
+		for len(minStack) > 0 && nums[minStack[len(minStack)-1]] > num {
 			minStack = minStack[:len(minStack)-1]
 		}
 		minStack = append(minStack, right)
-
-		for len(maxStack) > 0 && nums[maxStack[len(maxStack)-1]] < n {
+		for len(maxStack) > 0 && nums[maxStack[len(maxStack)-1]] < num {
 			maxStack = maxStack[:len(maxStack)-1]
 		}
 		maxStack = append(maxStack, right)
-
-		for len(minStack) > 0 && len(maxStack) > 0 && nums[maxStack[0]]-nums[minStack[0]] > limit {
+		if len(minStack) > 0 && len(maxStack) > 0 && nums[maxStack[0]]-nums[minStack[0]] > limit {
 			if left == minStack[0] {
 				minStack = minStack[1:]
 			}
@@ -28,7 +25,6 @@ func longestSubarray(nums []int, limit int) int {
 			}
 			left++
 		}
-
 		if right-left+1 > res {
 			res = right - left + 1
 		}
