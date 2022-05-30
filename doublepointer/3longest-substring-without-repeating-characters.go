86259c99139f55fb92927728fd235dc1f990d666
xujin <1319039722@qq.com>
2022-05-30 10:12:10 +0800

    3无重复字符的最长子串

---
+++ doublepointer/3longest-substring-without-repeating-characters.go
@@ -0,0 +1,27 @@
+package main
+
+//给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度
+func lengthOfLongestSubstring(s string) int {
+	left, window, res := 0, make(map[byte]int), 0
+
+	for right := 0; right < len(s); right++ {
+		c := s[right]
+		window[c]++
+
+		for left < right && window[c] > 1 {
+			d := s[left]
+			window[d]--
+			left++
+		}
+
+		if right-left+1 > res {
+			res = right - left + 1
+		}
+	}
+	return res
+}
+
+func main() {
+	res := lengthOfLongestSubstring("abcabcbb")
+	print(res)
+}
