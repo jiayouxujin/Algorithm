b40f4b6db3dbf54f3c8c242dc95ce8aa41c9c7cc
xujin <1319039722@qq.com>
2022-05-30 21:38:05 +0800

    142-环形链表2

---
+++ doublepointer/142-linked-list-cycle-ii.go
@@ -0,0 +1,24 @@
+package main
+
+func detectCycle(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+
+		if fast == slow {
+			// find
+			fast = head
+			for fast != slow {
+				fast = fast.Next
+				slow = slow.Next
+			}
+			return fast
+		}
+	}
+	return nil
+}
+
+func main() {
+	print("hello world")
+}
