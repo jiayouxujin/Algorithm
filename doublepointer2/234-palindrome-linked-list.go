a88658f6b024437d45294cac5fc1c7b5f528e4b9
xujin <1319039722@qq.com>
2022-06-07 09:32:46 +0800

    234-回文链表

---
+++ doublepointer2/234-palindrome-linked-list.go
@@ -0,0 +1,34 @@
+package main
+
+func isPalindrome(head *ListNode) bool {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	if fast != nil {
+		slow = slow.Next
+	}
+
+	left, right := head, reverse(slow)
+	for right != nil {
+		if left.Val != right.Val {
+			return false
+		}
+		left = left.Next
+		right = right.Next
+	}
+	return true
+}
+
+func reverse(head *ListNode) *ListNode {
+	var pre *ListNode
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = next
+	}
+	return pre
+}
