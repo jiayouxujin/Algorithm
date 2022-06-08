caf07768a4b528b977d25ce301e00c23463499b1
xujin <1319039722@qq.com>
2022-06-08 10:00:21 +0800

    4 二维数组中查找

--- doublepointer2/4-er-wei-shu-zu-zhong-de-cha-zhao-lcof.go
+++ doublepointer2/4-er-wei-shu-zu-zhong-de-cha-zhao-lcof.go
@@ -1,1 +1,19 @@
 package main
+
+func findNumberIn2DArray(matrix [][]int, target int) bool {
+	if matrix == nil || len(matrix) == 0 || len(matrix[0]) == 0 {
+		return false
+	}
+	row, col := 0, len(matrix[0])-1
+	for row < len(matrix) && col >= 0 {
+		if matrix[row][col] == target {
+			return true
+		}
+		if matrix[row][col] > target {
+			col--
+		} else {
+			row++
+		}
+	}
+	return false
+}
