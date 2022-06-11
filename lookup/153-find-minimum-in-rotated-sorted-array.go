3ae473017fa5c570604bdadc57c9f8f4d1ea42ba
xujin <1319039722@qq.com>
2022-06-12 07:55:37 +0800

    153-旋转数组中最小值

--- lookup/153-find-minimum-in-rotated-sorted-array.go
+++ lookup/153-find-minimum-in-rotated-sorted-array.go
@@ -1,19 +1,19 @@
 package main
 
-func findPeakElement(nums []int) int {
+func findMin(nums []int) int {
 	left, right := 0, len(nums)-1
 	for left < right {
 		mid := left + (right-left)>>1
-		if nums[mid] > nums[mid+1] {
+		if nums[mid] < nums[right] {
 			right = mid
 		} else {
 			left = mid + 1
 		}
 	}
-	return left
+	return nums[left]
 }
 
 //func main() {
-//	nums := []int{1, 2, 3, 1}
-//	fmt.Printf("%v \n", findPeakElement(nums))
+//	nums := []int{3, 4, 5, 1, 2}
+//	fmt.Printf("%v \n", findMin(nums))
 //}
