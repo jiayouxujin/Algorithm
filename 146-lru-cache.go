c971a9bc5b11ee0ce43e0cb10735cd5dee41420e
xujin <1319039722@qq.com>
2022-07-05 20:55:35 +0800

    146-LRU cache

---
+++ 146-lru-cache.go
@@ -0,0 +1,88 @@
+package main
+
+type Node struct {
+	Key, Val   int
+	Prev, Next *Node
+}
+type LRUCache struct {
+	head, tail *Node
+	Keys       map[int]*Node
+	Cap        int
+}
+
+func Constructor(capacity int) LRUCache {
+	return LRUCache{Keys: make(map[int]*Node), Cap: capacity}
+}
+
+func (this *LRUCache) Get(key int) int {
+	if node, ok := this.Keys[key]; ok {
+		this.Remove(node)
+		this.Add(node)
+		return node.Val
+	}
+	return -1
+}
+
+func (this *LRUCache) Put(key int, value int) {
+	if node, ok := this.Keys[key]; ok {
+		node.Val = value
+		this.Remove(node)
+		this.Add(node)
+		return
+	} else {
+		node = &Node{Key: key, Val: value}
+		this.Keys[key] = node
+		this.Add(node)
+	}
+	if len(this.Keys) > this.Cap {
+		delete(this.Keys, this.tail.Key)
+		this.Remove(this.tail)
+	}
+}
+
+func (this *LRUCache) Add(node *Node) {
+	node.Prev = nil
+	node.Next = this.head
+	if this.head != nil {
+		this.head.Prev = node
+	}
+	this.head = node
+	if this.tail == nil {
+		this.tail = node
+		this.tail.Next = nil
+	}
+}
+
+func (this *LRUCache) Remove(node *Node) {
+	if node == this.head {
+		this.head = node.Next
+		node.Next = nil
+		return
+	}
+	if node == this.tail {
+		this.tail = node.Prev
+		node.Prev.Next = nil
+		node.Prev = nil
+		return
+	}
+	node.Prev.Next = node.Next
+	node.Next.Prev = node.Prev
+}
+
+//func main() {
+//	cache := Constructor(2)
+//	cache.Put(1, 1)
+//	cache.Put(2, 2)
+//	fmt.Printf("%v \n", cache.Get(1))
+//	cache.Put(3, 3)
+//	fmt.Printf("%v \n", cache.Get(2))
+//	cache.Put(4, 4)
+//	fmt.Printf("%v \n", cache.Get(1))
+//}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * obj := Constructor(capacity);
+ * param_1 := obj.Get(key);
+ * obj.Put(key,value);
+ */
