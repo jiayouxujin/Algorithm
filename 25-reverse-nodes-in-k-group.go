f14e48558470dbc2364f24a57532c63ec938644f
xujin <1319039722@qq.com>
2022-07-08 22:14:21 +0800

    25

---
+++ 25-reverse-nodes-in-k-group.go
@@ -0,0 +1,29 @@
+package main
+
+func reverseKGroup(head *ListNode, k int) *ListNode {
+	if head == nil {
+		return nil
+	}
+	a, b := head, head
+	for i := 0; i < k; i++ {
+		if b == nil {
+			return head
+		}
+		b = b.Next
+	}
+	newHead := reverse(a, b)
+	a.Next = reverseKGroup(b, k)
+	return newHead
+}
+
+func reverse(a, b *ListNode) *ListNode {
+	var prev *ListNode
+	cur, next := a, a
+	for cur != b {
+		next = cur.Next
+		cur.Next = prev
+		prev = cur
+		cur = next
+	}
+	return prev
+}
