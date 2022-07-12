31dc473782407046db7a3fe3a4242c68d1a89441
xujin <1319039722@qq.com>
2022-07-12 13:44:05 +0800

    102-二叉树层次遍历

---
+++ algorithm/102-binary-tree-level-order-traversal.go
@@ -0,0 +1,36 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func levelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return [][]int{}
+	}
+
+	res := make([][]int, 0)
+	queue := make([]*TreeNode, 0)
+	queue = append(queue, root)
+
+	for len(queue) > 0 {
+		sz := len(queue)
+		tmp := make([]int, 0)
+		for i := 0; i < sz; i++ {
+			cur := queue[0]
+			queue = queue[1:]
+
+			tmp = append(tmp, cur.Val)
+			if cur.Left != nil {
+				queue = append(queue, cur.Left)
+			}
+			if cur.Right != nil {
+				queue = append(queue, cur.Right)
+			}
+		}
+		res = append(res, tmp)
+	}
+	return res
+}
