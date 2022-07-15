4fea92e22d5848ad4bbc6e578b5c98f344dc02d7
xujin <1319039722@qq.com>
2022-07-15 13:55:57 +0800

    206-反转链表

---
+++ algorithm/206-reverse-linked-list.go
@@ -0,0 +1,18 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	var pre *ListNode
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = next
+	}
+	return pre
+}
