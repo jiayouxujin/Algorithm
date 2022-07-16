7cf7f8b28a8c12bc06f645839dfc7887e38425bf
xujin <1319039722@qq.com>
2022-07-16 17:23:06 +0800

    33-搜索旋转数组

---
+++ algorithm/33-search-in-rotated-sorted-array.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left < right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[left] <= nums[mid] {
+			if target < nums[mid] && target >= nums[left] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if target > nums[mid] && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	if nums[left] == target {
+		return left
+	}
+	return -1
+}
+
+func main() {
+	nums, target := []int{4, 5, 6, 7, 0, 1, 2}, 0
+	fmt.Printf("%v \n", search(nums, target))
+}
