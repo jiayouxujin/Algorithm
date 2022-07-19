dfb154a2355ddd153dc47fdb3ac8c43124c18953
xujin <1319039722@qq.com>
2022-07-19 13:27:21 +0800

    300-最长上升子序列

---
+++ 300-longest-increasing-subsequence.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func lengthOfLIS(nums []int) int {
+	if nums == nil || len(nums) == 0 {
+		return 0
+	}
+	dp := make([]int, len(nums))
+	dp[0] = 1
+
+	for i := 1; i < len(nums); i++ {
+		dp[i] = 1
+		for j := 0; j < i; j++ {
+			if nums[i] > nums[j] {
+				dp[i] = max(dp[i], dp[j]+1)
+			}
+		}
+	}
+
+	res := 0
+	for i := 0; i < len(dp); i++ {
+		res = max(res, dp[i])
+	}
+	return res
+}
+
+func main() {
+	nums := []int{10, 9, 2, 5, 3, 7, 101, 18}
+	fmt.Printf("%v \n", lengthOfLIS(nums))
+}
