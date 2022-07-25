b433236a34f1a800c46c2ed565fc49fc7bcb768b
xujin <1319039722@qq.com>
2022-07-25 10:16:26 +0800

    1143-最长公共子序列

---
+++ 1143-longest-common-subsequence.go
@@ -0,0 +1,23 @@
+package main
+
+func longestCommonSubsequence(text1 string, text2 string) int {
+	if text1 == "" || text2 == "" {
+		return 0
+	}
+	m, n := len(text1), len(text2)
+	dp := make([][]int, m+1)
+	for i := 0; i <= m; i++ {
+		dp[i] = make([]int, n+1)
+	}
+
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[m][n]
+}
