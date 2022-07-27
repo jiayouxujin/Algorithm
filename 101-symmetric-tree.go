c64259f5826b622d250c1e5940d6fceb1b8f0830
xujin <1319039722@qq.com>
2022-07-27 09:17:43 +0800

    101-对称二叉树

---
+++ 101-symmetric-tree.go
@@ -0,0 +1,18 @@
+package main
+
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	return check(root.Left, root.Right)
+}
+
+func check(left, right *TreeNode) bool {
+	if left == nil || right == nil {
+		return left == right
+	}
+	if left.Val != right.Val {
+		return false
+	}
+	return check(left.Right, right.Left) && check(left.Left, right.Right)
+}
