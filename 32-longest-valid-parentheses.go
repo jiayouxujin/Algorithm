e793706d086a4c9ba34e52921e1b37bbc51f6ec4
xujin <1319039722@qq.com>
2022-08-07 09:30:26 +0800

    32-最长有效括号

--- 32-longest-valid-parentheses.go
+++ 32-longest-valid-parentheses.go
@@ -11,14 +11,13 @@ func longestValidParentheses(s string) int {
 				leftIndex := stk[len(stk)-1]
 				stk = stk[:len(stk)-1]
 
-				len := 1 + i - leftIndex + dp[leftIndex]
+				len := i + 1 - leftIndex + dp[leftIndex]
 				dp[i+1] = len
 			} else {
 				dp[i+1] = 0
 			}
 		}
 	}
-
 	res := 0
 	for i := 0; i < len(dp); i++ {
 		res = max(res, dp[i])
