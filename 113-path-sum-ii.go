33f4bd020492fd009d708cf60dae1fa9095b7db6
xujin <1319039722@qq.com>
2022-08-09 09:15:00 +0800

    113-路径总和2

--- 113-path-sum-ii.go
+++ 113-path-sum-ii.go
@@ -2,21 +2,31 @@ package main
 
 func pathSum(root *TreeNode, targetSum int) [][]int {
 	var res [][]int
-	return helper(root, targetSum, res, []int{})
+	helper(root, targetSum, &res, []int{})
+	return res
 }
 
-func helper(root *TreeNode, sum int, res [][]int, tmp []int) [][]int {
+func helper(root *TreeNode, sum int, res *[][]int, tmp []int) {
 	if root == nil {
-		return res
+		return
 	}
 	sum -= root.Val
-	tmp = append(tmp, root.Val)
-	if root.Left == nil && root.Right == nil && sum == 0 {
-		res = append(res, append([]int{}, tmp...))
-		tmp = tmp[:len(tmp)-1]
+
+	if root.Left == nil && root.Right == nil {
+		if sum == 0 {
+			tmp = append(tmp, root.Val)
+			t := make([]int, len(tmp))
+			copy(t, tmp)
+			*res = append(*res, t)
+		}
+		return
 	}
 
-	res = helper(root.Left, sum, res, tmp)
-	res = helper(root.Right, sum, res, tmp)
-	return res
+	tmp = append(tmp, root.Val)
+	helper(root.Left, sum, res, tmp)
+	tmp = tmp[:len(tmp)-1]
+
+	tmp = append(tmp, root.Val)
+	helper(root.Right, sum, res, tmp)
+	tmp = tmp[:len(tmp)-1]
 }
