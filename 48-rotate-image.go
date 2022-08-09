0dd161e91f4b1a3350debd2978939ebcd25423ad
xujin <1319039722@qq.com>
2022-08-09 13:19:07 +0800

    48-旋转图像

---
+++ 48-rotate-image.go
@@ -0,0 +1,13 @@
+package main
+
+func rotate(matrix [][]int) {
+	n := len(matrix)
+	for i := 0; i < n; i++ {
+		for j := i; j < n; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+	for i := 0; i < n; i++ {
+		reverseArray(matrix[i])
+	}
+}
