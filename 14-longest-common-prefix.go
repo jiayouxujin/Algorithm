1ee04b17a339f777d14b015dec52a915e7fe85a7
xujin <1319039722@qq.com>
2022-08-10 10:05:42 +0800

    14-最长公共前缀

---
+++ 14-longest-common-prefix.go
@@ -0,0 +1,15 @@
+package main
+
+func longestCommonPrefix(strs []string) string {
+	prefix := strs[0]
+
+	for i := 1; i < len(strs); i++ {
+		for j := 0; j < len(prefix); j++ {
+			if len(strs[i]) <= j || strs[i][j] != prefix[j] {
+				prefix = prefix[:j]
+				break
+			}
+		}
+	}
+	return prefix
+}
