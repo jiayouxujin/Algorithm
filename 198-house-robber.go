822cd7da64cf8b7eae1f8a5b7793c894a37784cb
xujin <1319039722@qq.com>
2022-08-15 16:10:37 +0800

    198-打家劫舍

---
+++ 198-house-robber.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func rob(nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return nums[0]
+	}
+
+	dp := make([]int, n)
+	dp[0], dp[1] = nums[0], max(nums[1], nums[0])
+	for i := 2; i < n; i++ {
+		dp[i] = max(dp[i-1], nums[i]+dp[i-2])
+	}
+	return dp[n-1]
+}
+
+func main() {
+	nums := []int{1, 2, 3, 1}
+	fmt.Printf("%v \n", rob(nums))
+}
