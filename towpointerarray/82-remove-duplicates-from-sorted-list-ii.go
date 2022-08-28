aa24fdfb71a74fe0787d002d17c7c9e5346442f3
xujin <1319039722@qq.com>
2022-08-28 10:38:25 +0800

    82-删除链表中的重复元素

---
+++ towpointerarray/82-remove-duplicates-from-sorted-list-ii.go
@@ -0,0 +1,22 @@
+package main
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	dummyHead := &ListNode{Next: head}
+	p := dummyHead
+	for p.Next != nil {
+		q := p.Next.Next
+		for q != nil && p.Next.Val == q.Val {
+			q = q.Next
+		}
+		if q == p.Next.Next {
+			p = p.Next
+		} else {
+			p.Next = q
+		}
+	}
+	return dummyHead.Next
+}
