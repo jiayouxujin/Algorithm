fa7e3c79cd667242b7cea6b307807e7db1651d69
xujin <1319039722@qq.com>
2023-02-13 21:00:16 +0800

    200-numIslands

---
+++ prepare/200-number-of-islands.go
@@ -0,0 +1,25 @@
+package main
+
+func numIslands(grid [][]byte) int {
+	res := 0
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			if grid[i][j] == '1' {
+				res++
+				dfs(&grid, i, j)
+			}
+		}
+	}
+	return res
+}
+
+func dfs(grid *[][]byte, i, j int) {
+	if i < 0 || i >= len(*grid) || j < 0 || j >= len((*grid)[0]) || (*grid)[i][j] == '0' {
+		return
+	}
+	(*grid)[i][j] = '0'
+	dfs(grid, i+1, j)
+	dfs(grid, i-1, j)
+	dfs(grid, i, j+1)
+	dfs(grid, i, j-1)
+}
