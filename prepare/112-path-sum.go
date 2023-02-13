21e38b79a1790b9c95e71915bec591ea5cfd2f15
xujin <1319039722@qq.com>
2023-02-13 21:06:10 +0800

    112-hasPathSum

---
+++ prepare/112-path-sum.go
@@ -0,0 +1,11 @@
+package main
+
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+	if root.Left == nil && root.Right == nil && targetSum == root.Val {
+		return true
+	}
+	return hasPathSum(root.Left, targetSum-root.Val) || hasPathSum(root.Right, targetSum-root.Val)
+}
