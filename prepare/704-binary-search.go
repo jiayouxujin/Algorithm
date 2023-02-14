b88cff9512f602b28fc8e0a0d93f050ebe01e27f
xujin <1319039722@qq.com>
2023-02-14 13:52:50 +0800

    704-search

---
+++ prepare/704-binary-search.go
@@ -0,0 +1,19 @@
+package main
+
+func search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left < right {
+		mid := (right-left)/2 + left
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	if nums[left] == target {
+		return left
+	}
+	return -1
+}
