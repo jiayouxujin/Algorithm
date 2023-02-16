2477095782fc79ca93d2410b365af435c9a596d5
xujin <1319039722@qq.com>
2023-02-16 16:17:15 +0800

    454-fourSumCount

---
+++ prepare/454-4sum-ii.go
@@ -0,0 +1,17 @@
+package main
+
+func fourSumCount(nums1 []int, nums2 []int, nums3 []int, nums4 []int) int {
+	m := make(map[int]int)
+	for _, n1 := range nums1 {
+		for _, n2 := range nums2 {
+			m[n1+n2] = 1
+		}
+	}
+	res := 0
+	for _, n3 := range nums3 {
+		for _, n4 := range nums4 {
+			res += m[-n3-n4]
+		}
+	}
+	return res
+}
