818faed03ee505657ae9e5689f8b9006a1c8815e
xujin <1319039722@qq.com>
2023-02-16 16:53:03 +0800

    j09-yong-liang-ge-zhan-shi-xian-dui-lie-lcof

---
+++ prepare/j09-yong-liang-ge-zhan-shi-xian-dui-lie-lcof.go
@@ -0,0 +1,29 @@
+package main
+
+type CQueue struct {
+	in, out []int
+}
+
+func Constructor() CQueue {
+	return CQueue{
+		in:  make([]int, 0),
+		out: make([]int, 0),
+	}
+}
+
+func (this *CQueue) AppendTail(value int) {
+	this.in = append(this.in, value)
+}
+
+func (this *CQueue) DeleteHead() int {
+	if len(this.out) == 0 {
+		for i := len(this.in) - 1; i >= 0; i-- {
+			v := this.in[i]
+			this.in = this.in[:len(this.in)-1]
+			this.out = append(this.out, v)
+		}
+	}
+	v := this.out[len(this.out)-1]
+	this.out = this.out[:len(this.out)-1]
+	return v
+}
