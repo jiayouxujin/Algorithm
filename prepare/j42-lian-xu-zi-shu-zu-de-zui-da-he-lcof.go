978c91ed68c36bc4a5a1010679d6c8521ffaa8a5
xujin <1319039722@qq.com>
2023-02-16 15:53:57 +0800

    j42-maxSubArray2

---
+++ prepare/j42-lian-xu-zi-shu-zu-de-zui-da-he-lcof.go
@@ -0,0 +1,13 @@
+package main
+
+func maxSubArray2(nums []int) int {
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	res := nums[0]
+
+	for i := 1; i < len(nums); i++ {
+		dp[i] = max(dp[i-1]+nums[i], nums[i])
+		res = max(res, dp[i])
+	}
+	return res
+}
