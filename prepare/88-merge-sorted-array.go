0b0bc81ab811ea674208bca04dc9d7a904be2ce4
xujin <1319039722@qq.com>
2023-02-18 10:55:46 +0800

    88-merge

---
+++ prepare/88-merge-sorted-array.go
@@ -0,0 +1,26 @@
+package main
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	l := len(nums1) - 1
+	m--
+	n--
+	for m >= 0 && n >= 0 {
+		if nums1[m] > nums2[n] {
+			nums1[l] = nums1[m]
+			m--
+		} else {
+			nums1[l] = nums2[n]
+			n--
+		}
+		l--
+	}
+	if m >= 0 {
+		return
+	} else {
+		for n >= 0 {
+			nums1[l] = nums2[n]
+			n--
+			l--
+		}
+	}
+}
