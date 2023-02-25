74a19e3cf81d5fa6f97d3a0768f96ea1b8b2dd1d
xujin <1319039722@qq.com>
2023-02-25 10:53:08 +0800

    121-maxProfit1

---
+++ prepare/121-best-time-to-buy-and-sell-stock.go
@@ -0,0 +1,16 @@
+package main
+
+func maxProfit1(prices []int) int {
+	dp := make([][]int, len(prices))
+	for i := 0; i < len(prices); i++ {
+		dp[i] = make([]int, 2)
+	}
+	dp[0][0] = 0
+	dp[0][1] = -prices[0]
+
+	for i := 1; i < len(prices); i++ {
+		dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
+		dp[i][1] = max(dp[i-1][1], -prices[i])
+	}
+	return dp[len(prices)-1][0]
+}
