f71c882d99255827aad81d13a13b6d011ec5ef5c
xujin <1319039722@qq.com>
2023-02-25 16:21:09 +0800

    35-searchInsert

---
+++ niuke/35-search-insert-position.go
@@ -0,0 +1,16 @@
+package main
+
+func searchInsert(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := (right-left)/2 + left
+		if nums[mid] < target {
+			left = mid + 1
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else {
+			return mid
+		}
+	}
+	return left
+}
