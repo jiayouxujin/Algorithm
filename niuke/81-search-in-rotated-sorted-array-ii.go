1603f2fd56a3892f66ee67727e7b0a37999b3390
xujin <1319039722@qq.com>
2023-02-26 14:04:42 +0800

    81-search2

---
+++ niuke/81-search-in-rotated-sorted-array-ii.go
@@ -0,0 +1,27 @@
+package main
+
+func search2(nums []int, target int) bool {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return true
+		}
+		if nums[left] == nums[mid] {
+			left++
+		} else if nums[left] < nums[mid] {
+			if nums[left] <= target && nums[mid] > target {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if nums[right] >= target && target > nums[mid] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	return false
+}
