1de837f364b9a8861e252ad15e036244978cb453
xujin <1319039722@qq.com>
2023-03-23 16:33:12 +0800

    206-reverseList2

---
+++ interview/206-reverse-linked-list-2.go
@@ -0,0 +1,11 @@
+package main
+
+func reverseList2(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	last := reverseList2(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return last
+}
